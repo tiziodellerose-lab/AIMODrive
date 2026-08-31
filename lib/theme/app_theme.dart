d3953c0f6bbf01f7a807655dc7d1afcd42c32f99
tiziodellerose-lab <tiziodellerose@gmail.com>
2026-08-31 12:40:15 +0200

    Create app_theme.dart
    gestione del tema

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData get lightTheme {
+    return ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: Colors.blue,
+      brightness: Brightness.light,
+    );
+  }
+
+  static ThemeData get darkTheme {
+    return ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: Colors.blue,
+      brightness: Brightness.dark,
+    );
+  }
+}
