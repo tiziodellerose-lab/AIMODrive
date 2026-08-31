a84e29a98c1210d0e387da9cc18e7979d8b421b1
tiziodellerose-lab <tiziodellerose@gmail.com>
2026-08-31 13:02:11 +0200

    Create constants.dart

---
+++ lib/core/constants.dart
@@ -0,0 +1,5 @@
+class AppConstants {
+  static const String appName = 'AIMODrive';
+  static const String version = '0.1.0';
+  static const String noFilesMessage = 'Nessun file presente. Aggiungi un provider Cloud.';
+}
