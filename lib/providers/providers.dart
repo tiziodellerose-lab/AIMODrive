1005f6ffbceb7a5c218e1d2a7fb09efbb78430d5
tiziodellerose-lab <tiziodellerose@gmail.com>
2026-09-01 08:57:22 +0200

    Update providers.dart

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -26,3 +26,41 @@ class CloudAccountsNotifier extends StateNotifier<List<CloudAccount>> {
     }
   }
 }
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../models/cloud_account.dart';
+import '../services/google_drive_service.dart';
+import '../services/mega_service.dart'; // Aggiunto import
+
+final googleDriveServiceProvider = Provider((ref) => GoogleDriveService());
+final megaServiceProvider = Provider((ref) => MegaService()); // Nuovo provider
+
+final cloudAccountsProvider = StateNotifierProvider<CloudAccountsNotifier, List<CloudAccount>>((ref) {
+  return CloudAccountsNotifier(
+    ref.watch(googleDriveServiceProvider),
+    ref.watch(megaServiceProvider), // Passiamo anche Mega
+  );
+});
+
+class CloudAccountsNotifier extends StateNotifier<List<CloudAccount>> {
+  final GoogleDriveService _driveService;
+  final MegaService _megaService;
+
+  CloudAccountsNotifier(this._driveService, this._megaService) : super([]);
+
+  // ... (Mantieni il vecchio metodo signInWithGoogle se vuoi, o rimuovilo)
+
+  Future<bool> signInWithMega(String email, String password) async {
+    final success = await _megaService.loginWithCredentials(email, password);
+    
+    if (success) {
+      final newAccount = CloudAccount(
+        id: DateTime.now().millisecondsSinceEpoch.toString(), // ID fittizio per ora
+        providerName: 'Mega',
+        email: email,
+      );
+      state = [...state, newAccount];
+      return true;
+    }
+    return false;
+  }
+}
