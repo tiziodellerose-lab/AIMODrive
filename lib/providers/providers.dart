edb59c9387efe4766e75dde12a5c9b97fb34dd13
tiziodellerose-lab <tiziodellerose@gmail.com>
2026-09-01 08:28:25 +0200

    Update providers.dart

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -1,8 +1,28 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../models/cloud_account.dart';
+import '../services/google_drive_service.dart';
 
-// Provider iniziale per gestire la lista degli account cloud connessi
-final cloudAccountsProvider = StateProvider<List<CloudAccount>>((ref) {
-  // In questa Release 0.1 partiamo con una lista vuota
-  return [];
+final googleDriveServiceProvider = Provider((ref) => GoogleDriveService());
+
+final cloudAccountsProvider = StateNotifierProvider<CloudAccountsNotifier, List<CloudAccount>>((ref) {
+  return CloudAccountsNotifier(ref.watch(googleDriveServiceProvider));
 });
+
+class CloudAccountsNotifier extends StateNotifier<List<CloudAccount>> {
+  final GoogleDriveService _driveService;
+
+  CloudAccountsNotifier(this._driveService) : super([]);
+
+  Future<void> signInWithGoogle() async {
+    final success = await _driveService.authenticate();
+    if (success && _driveService.currentUser != null) {
+      final user = _driveService.currentUser!;
+      final newAccount = CloudAccount(
+        id: user.id,
+        providerName: 'Google Drive',
+        email: user.email,
+      );
+      state = [...state, newAccount];
+    }
+  }
+}
